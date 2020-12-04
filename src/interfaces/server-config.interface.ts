7c4b49afa8ba28a8cf0933d1066016698e4d1bac
akanass <akanass@icloud.com>
2020-12-04 14:35:27 +0100

    feat(views): add views engine and static assets elements

--- src/interfaces/server-config.interface.ts
+++ src/interfaces/server-config.interface.ts
@@ -2,4 +2,17 @@ export interface ServerConfig {
   protocol: string;
   host: string;
   port: number;
+  views: ViewsConfig;
+  assets: AssetsConfig;
+}
+
+export interface ViewsConfig {
+  templatesPath: string;
+  layout: string;
+  title: string;
+}
+
+export interface AssetsConfig {
+  rootPath: string;
+  prefix: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ServerConfig } from './interfaces/server-config.interface';
 import * as Config from 'config';
+import { join } from 'path';
 
 async function bootstrap(config: ServerConfig) {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -18,6 +19,38 @@ async function bootstrap(config: ServerConfig) {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // set static assets
+  app.useStaticAssets({
+    root: join(__dirname, config.assets.rootPath),
+    prefix: config.assets.prefix,
+  });
+
+  // set view engine
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, config.views.templatesPath),
+    layout: config.views.layout,
+    includeViewExtension: true,
+    options: {
+      async: true,
+      useHtmlMinifier: require('html-minifier'),
+      htmlMinifierOptions: {
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    },
+    defaultContext: {
+      title: config.views.title,
+    }
+  });
 
   // launch server
   await app.listen(config.port, config.host);

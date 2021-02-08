d420768c4f18a00f32289779fd814eba17a1ce5e
akanass <akanass@icloud.com>
2021-02-08 16:09:33 +0100

    feat(security): clean session when error occurred to restart process

--- src/filters/view-error-page.filter.ts
+++ src/filters/view-error-page.filter.ts
@@ -8,8 +8,9 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { AppService } from '../app.service';
+import { SecurityService } from '../security/security.service';
 
 @Catch(UnauthorizedException, NotFoundException, ForbiddenException, InternalServerErrorException)
 export class ViewErrorPageFilter implements ExceptionFilter {
@@ -18,8 +19,9 @@ export class ViewErrorPageFilter implements ExceptionFilter {
    *
    * @param {AppService} _appService dependency injection of AppService instance
    * @param {Logger} _logger dependency injection of Logger instance
+   * @param {SecurityService} _securityService dependency injection of SecurityService instance
    */
-  constructor(private readonly _appService: AppService, private readonly _logger: Logger) {
+  constructor(private readonly _appService: AppService, private readonly _logger: Logger, private readonly _securityService: SecurityService) {
   }
 
   /**
@@ -31,9 +33,14 @@ export class ViewErrorPageFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const response: any = ctx.getResponse<FastifyReply>();
+    const request: FastifyRequest = ctx.getRequest<FastifyRequest>();
     const status = exception.getStatus();
 
+    // log error
     this._logger.error(exception.getResponse().message, JSON.stringify(exception.getResponse()),'ViewErrorPageFilter');
+
+    // delete session
+    this._securityService.cleanSession(request.session);
 
     // display error page
     response.status(status).view('error', this._appService.getMetadata('error'));
